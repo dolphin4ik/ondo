b7f1885968b547dca6a13df566b0e790f2dc828c
Denis <denyzhirkov@yandex.ru>
2017-10-27 12:42:47 +0300

    1.2.0

--- README.md
+++ README.md
@@ -1,18 +1,32 @@
 # ondo
+Event Emitter
+
+No dependencies. Works everywhere.
+
 **on** something **do** something
 
 Get it from [CDN](https://unpkg.com/ondo@latest)
 Try it on [RunKit](https://runkit.com/58e8a7572e03700014c19610/58e8a7572e03700014c19611)
 
-```js
-let e = ondo();
-
-e.on('hello', function(){ console.log('hello'); })
- .on('world', function(){ console.log('world'); })
- .on('greet',function(){ this.do('hello').do('world'); });
- 
-e.do('greet'); // -> hello world
-```
-.on(*event*, *callback*,*context*)
+.on(*event*, *callback*, *context*)
 
 .do(*event*, *context*)
+
+```js
+let event = require("ondo")();
+
+event.on('hello',() => console.log('hello'));
+event.on('world',() => console.log('world'));
+event.do('hello').do('world'); //hello .. world
+
+event.on('greet', function(){ this.do('hello').do('world') });
+event.do('greet'); //hello .. world
+
+event.off('greet');
+event.do('greet'); //"No 'greet' event registered"
+
+
+event.on("withContext", function(){ console.log(this) }, {"installed": "context"});
+event.do("withContext"); //Object { "installed": "context" }
+event.do("withContext", {"modified": "context"}); //Object { "modified": "context" }
+```
--- ondo.js
+++ ondo.js
@@ -1,52 +1,49 @@
 ;(function (root, factory, name){
-    if (typeof define === 'function' && define.amd){
-        define([], factory);
-    }else if(typeof module === 'object' && module.exports){
-        module.exports = factory();
-    }else{
-        root[name] = factory();
-    }
+	if (typeof define === 'function' && define.amd){
+		define([], factory);
+	}else if(typeof module === 'object' && module.exports){
+		module.exports = factory();
+	}else{
+		root[name] = factory();
+	}
 })(this, function (){
-    return function(){
+	return function(){
 
-    	var events = {};
-    	var ondo = {
-    		on: function(name, handler, context){
-    			(events[name] || (events[name] = [])).push({
-    				"handler": handler || false,
-    				"context": context || this
-    			});
-    		return this;
-    		},
-    		do: function(name, context){
-    			if(!events[name]){
-    				throw "No '" + name + "' event registered";
-    			} else {
-    				var args = [].slice.call(arguments, 2);
-    				events[name].forEach(function(e){
-    					if(e.handler){
-    						e.handler.apply((context || e.context), args);
-    					}
-    				});
-                    if(events['*']){
-                        events['*'].forEach(function(e){
-                            if(e.handler){
-                                e.handler.apply((context || e.context), args);
-                            }
-                        });
-                    }
-    			}
-    			return this;
-    		},
-    		off: function(name, context){
-                var args = [].slice.call(arguments, 1);
-                args.unshift('off');
-    			delete events[name];
-                this.do.apply(null, args);
-                return this;
-    		}
-    	}
-    	return ondo;
+		var events = {};
+		var ondo = {
+			on: function(name, handler, context){
+				(events[name] || (events[name] = [])).push({
+					"handler": handler || false,
+					"context": context || this
+				});
+			return this;
+			},
+			do: function(name, context){
+				if(!events[name]){
+					console.error( "No '" + name + "' event registered" );
+				} else {
+					var args = [].slice.call(arguments, 2);
+					events[name].forEach(function(e){
+						if(e.handler){
+							e.handler.apply(context || e.context, args);
+						}
+					});
+					if(events['*']){
+						events['*'].forEach(function(e){
+							if(e.handler){
+								e.handler.apply(context || e.context, args);
+							}
+						});
+					}
+				}
+				return this;
+			},
+			off: function(name, context){
+				delete events[name];
+				return this;
+			}
+		}
+		return ondo;
 
-    }
+	}
 }, "ondo");
